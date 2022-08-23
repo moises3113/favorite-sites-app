be63c0b7f03f6b6d6534952e65f6cc06f88c4f21
moises3113 <moises.garcia3113@gmail.com>
2022-08-23 16:31:00 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,1 @@
-module.exports = {
-    database:{
-        host: '137.184.86.139',
-        user:'mgarcia',
-        password:'SitioPruebas2022m',
-        port:3306,
-        database:'database_links'
-    }    
-}
+
